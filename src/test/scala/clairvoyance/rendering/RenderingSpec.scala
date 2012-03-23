b24de239699c0df85be25cbf37f037c6d8da14d0
Team Five-Oh <team-five@sky.com>
2012-03-23 15:04:52 +0000

    Added additional spec for Rendering

---
+++ src/test/scala/clairvoyance/rendering/RenderingSpec.scala
@@ -0,0 +1,49 @@
+package clairvoyance.rendering
+
+import xml.PrettyPrinter
+import org.specs2.clairvoyance.{ProducesCapturedInputsAndOutputs, ClairvoyantSpec}
+import org.specs2.clairvoyance.rendering.{CustomRendering, Rendering}
+
+class RenderingSpec extends ClairvoyantSpec {
+
+  "The Renderer" should {
+    "render objects as strings" in new context {
+      rendered(brain) must_== <span>{brain.toString}</span>
+    }
+
+    "render XML as escaped XML" in new context {
+      rendered(xml) must_== <span>{formattedXml}</span>
+    }
+
+    "use the custom renderer" in new context {
+      rendered(ToBeCustomRendered("nine times")) must_== <span>nine times</span>
+    }
+  }
+
+  trait context extends ClairvoyantContext with ProducesCapturedInputsAndOutputs  {
+    val rendering = new Rendering(Some(new CustomRenderer))
+    val brain = Brain(120)
+
+    override def capturedInputsAndOutputs = Seq(this)
+
+    val xml = <xml>someXml</xml>
+    val formattedXml = new PrettyPrinter(80, 2).formatNodes(xml)
+
+    def rendered(thingToRender: Any) = {
+      captureValue("Input" -> thingToRender)
+      val output = rendering.renderToXml(thingToRender)
+      captureValue("Output" -> output)
+      output
+    }
+  }
+
+  case class Brain(iq: Int)
+
+  case class ToBeCustomRendered(stringToRender: String)
+
+  class CustomRenderer extends CustomRendering {
+    def customRendering = {
+      case ToBeCustomRendered(x) => x
+    }
+  }
+}
