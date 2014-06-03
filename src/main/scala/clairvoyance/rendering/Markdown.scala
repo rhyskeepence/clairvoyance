c601f201b6af4cb03a0806a484de1723f8df5208
Franck Rasolo <franck.rasolo@gmail.com>
2014-06-03 08:49:21 +0100

    issue #5 - Further inlined specs2's Markdown.markdownToXhtml

--- src/main/scala/clairvoyance/rendering/Markdown.scala
+++ src/main/scala/clairvoyance/rendering/Markdown.scala
@@ -1,15 +1,47 @@
 package clairvoyance.rendering
 
 import org.pegdown.{Extensions, LinkRenderer, PegDownProcessor, ToHtmlSerializer}
+import org.pegdown.ast.CodeNode
 import scala.io.Source
+import scala.util.matching.Regex
 import scala.xml.NodeSeq
 import scala.xml.parsing.XhtmlParser
 
 object Markdown {
 
-  def markdownToXhtml(markdownText: String): NodeSeq = {
+  /** Inlined from org.specs2.text.Markdown */
+  def markdownToXhtml(markdown: String): NodeSeq = {
     val pegDown = new PegDownProcessor(Extensions.ALL & ~Extensions.QUOTES & ~Extensions.SMARTS, 2000L)
-    val html = new ToHtmlSerializer(new LinkRenderer).toHtml(pegDown.parseMarkdown(markdownText.replace("\\\\n", "\n").toCharArray))
-    XhtmlParser(Source.fromString(s"<text>$html</text>"))
+    val htmlSerializer = new ToHtmlSerializer(new LinkRenderer) {
+      override def visit(node: CodeNode): Unit = {
+        val text = node.getText
+        if (text.contains("\n"))
+          printer
+            .print("<pre>")
+            .print("""<code class="prettyprint">""")
+            .printEncoded(text.removeFirst("\n"))
+            .print("</code>")
+            .print("</pre>")
+        else
+          printer
+            .print("""<code class="prettyprint">""")
+            .printEncoded(text)
+            .print("</code>")
+      }
+    }
+    val html = htmlSerializer.toHtml(pegDown.parseMarkdown(markdown.replace("\\\\n", "\n").toCharArray))
+    val htmlWithoutParagraph = if (!markdown.contains("\n") || markdown.trim.isEmpty) html.removeEnclosingXmlTag("p") else html
+    XhtmlParser(Source.fromString(s"<text>$htmlWithoutParagraph</text>"))
+  }
+
+  implicit def trimmed(s: String): Trimmed = new Trimmed(s)
+
+  class Trimmed(s: String) {
+    def removeEnclosingXmlTag(t: String) =
+      if (isEnclosing("<" + t, "</" + t + ">")) removeFirst("<" + t + ".*?>").trimEnd("</" + t + ">") else s
+
+    def isEnclosing(start: String, end: String) = s.startsWith(start) && s.endsWith(end)
+    def removeFirst(regex: String) = new Regex(regex).replaceFirstIn(s, "")
+    def trimEnd(end: String) = if (s.trim.endsWith(end)) s.trim.dropRight(end.size) else s.trim
   }
 }
