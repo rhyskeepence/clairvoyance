bdafd13aa01c903d8ccd0eae9dd8f15d211eb165
Franck Rasolo <franck.rasolo@gmail.com>
2014-06-04 20:29:05 +0100

    A little reformatting to avoid blank lines in literate specs

--- core/src/test/scala/clairvoyance/specs2/examples/ScalaCheckExample.scala
+++ core/src/test/scala/clairvoyance/specs2/examples/ScalaCheckExample.scala
@@ -9,32 +9,26 @@ class ScalaCheckExample extends ClairvoyantSpec with ScalaCheck {
   "String" should {
     "support startsWith" in new context {
       givenTwoStrings((a: String, b: String) =>
-        (a + b).startsWith(a) must beTrue
-      )
+        (a + b).startsWith(a) must beTrue)
     }
 
     "support endsWith" in new context {
       givenTwoStrings((a: String, b: String) =>
-        (a + b).endsWith(b) must beTrue
-      )
+        (a + b).endsWith(b) must beTrue)
     }
 
     "support concat" in new context {
       givenTwoStrings((a: String, b: String) =>
-        (a + b).length must beEqualTo(a.length + b.length)
-      )
+        (a + b).length must beEqualTo(a.length + b.length))
     }
 
     "support substring" in new context {
       givenTwoStrings((a: String, b: String) =>
-        (a + b).substring(a.length) must beEqualTo(b)
-      )
+        (a + b).substring(a.length) must beEqualTo(b))
     }
   }
 
   trait context extends ClairvoyantContext {
-    def givenTwoStrings(f: (String, String) => Boolean) = {
-      check(forAll { f })
-    }
+    def givenTwoStrings(f: (String, String) => Boolean) = check(forAll { f })
   }
 }
