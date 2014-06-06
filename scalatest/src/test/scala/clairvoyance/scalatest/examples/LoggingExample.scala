d4211e916d447e658be54207ec5470f789e8138a
Franck Rasolo <franck.rasolo@gmail.com>
2014-06-06 14:14:39 +0100

    issue #6 - Ported LoggingExample to ScalaTest

---
+++ scalatest/src/test/scala/clairvoyance/scalatest/examples/LoggingExample.scala
@@ -0,0 +1,74 @@
+package clairvoyance.scalatest.examples
+
+import clairvoyance.ProducesCapturedInputsAndOutputs
+import clairvoyance.scalatest.ClairvoyantContext
+import java.text.SimpleDateFormat
+import java.util.Date
+import org.scalatest.{Matchers, Spec, Suite}
+
+class LoggingExample extends Spec with DoomsdayContext with Matchers {
+
+  object `The coordinator must` {
+    def `invoke the Doomsday Device on the 21st of December 2012`() {
+      givenTheDateIs("21/12/2012")
+      whenTheCoordinatorRuns()
+      theDoomsdayDevice shouldBe unleashed
+    }
+  }
+}
+
+trait DoomsdayContext extends ClairvoyantContext with Matchers{ this: Suite =>
+  val theDoomsdayDevice = new StubDoomsdayDevice
+  val clock = new StubClock
+
+  override def capturedInputsAndOutputs = Seq(theDoomsdayDevice)
+
+  def givenTheDateIs(ddMMyyyy: String): Unit = clock.setDateTo(toDate(ddMMyyyy))
+  private def toDate(ddMMyyyy: String) = new SimpleDateFormat("dd/MM/yyyy").parse(ddMMyyyy)
+
+  def whenTheCoordinatorRuns(): Unit = new MasterCoordinator(theDoomsdayDevice, clock).runIt()
+
+  def unleashed = 'wasUnleashed
+
+  /* Below is the system under test */
+  class MasterCoordinator(doomsdayDevice: DoomsdayDevice, clock: Clock) {
+    val triggerDate = toDate("21/12/2012")
+
+    def runIt(): Unit = {
+      if (clock.currentTime equals triggerDate)
+        doomsdayDevice.unleashDestruction("Planet Earth")
+    }
+  }
+
+  trait DoomsdayDevice {
+    def unleashDestruction(target: String): Unit
+  }
+
+  trait Clock {
+    def currentTime: Date
+  }
+
+  class StubClock extends Clock {
+    def setDateTo(time: Date): Unit = interestingGivens += ("Current date" -> time)
+
+    def currentTime =
+      interestingGivens("Current date")
+        .map(_.asInstanceOf[Date])
+        .getOrElse(sys.error("No date set"))
+  }
+
+  class StubDoomsdayDevice extends DoomsdayDevice with ProducesCapturedInputsAndOutputs {
+    var wasUnleashed = false
+
+    def unleashDestruction(target: String): Unit = {
+      wasUnleashed = true
+      interestingGivens += ("Target" -> target)
+
+      captureValue("Doomsday Device Output" ->
+        <unleashDestruction>
+          <target>{target}</target>
+        </unleashDestruction>
+      )
+    }
+  }
+}
