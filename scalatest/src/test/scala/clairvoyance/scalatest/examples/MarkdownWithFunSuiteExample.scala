c1333cd4dd828d6c878ba1b5e97080ec4ee9ee72
Franck Rasolo <franck.rasolo@gmail.com>
2014-08-14 12:27:59 +0100

    Added Markdown example that uses FunSuite

---
+++ scalatest/src/test/scala/clairvoyance/scalatest/examples/MarkdownWithFunSuiteExample.scala
@@ -0,0 +1,42 @@
+package clairvoyance.scalatest.examples
+
+import org.scalatest.FunSuite
+
+class MarkdownWithFunSuiteExample extends FunSuite {
+
+  test( """The Triangle Number Calculator
+          |----
+          |
+          |###Rules
+          |* The n-th triangle number is the number of dots composing a triangle with n dots on a side, and is equal to the sum of the n natural numbers from 1 to n
+          |* [Further reading](http://en.wikipedia.org/wiki/Triangular_number)
+          |----
+          |
+          |Here we calculate the 6th triangle number, represented by the following diagram:
+          |```
+          |       *
+          |      **
+          |     ***
+          |    ****
+          |   *****
+          |  ******
+          |```
+        """.stripMargin) {
+    triangle(6) == 21
+  }
+
+  test( """The Calculator
+          |----
+          |
+          |### Rules
+          |* The factorial of n is the product of all positive integers less than or equal to n
+          |* *Non-negative* integers only
+          |* [Further reading](http://en.wikipedia.org/wiki/Factorial)
+        """.stripMargin) {
+    factorial(3) == 6
+  }
+
+  def factorial(n: Int): Int = if (n == 1) n else n * factorial(n - 1)
+
+  def triangle(n: Int): Int = n * (n + 1) / 2
+}
