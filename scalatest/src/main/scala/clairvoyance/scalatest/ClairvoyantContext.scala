228cbb6e28b1c402846ce15bb15fd60c5182754b
franck.rasolo <franck.rasolo@hsbc.com>
2014-08-19 16:41:58 +0100

    Simplified ClairvoyantContext by no longer mixing in BeforeAndAfterEachTestData

--- scalatest/src/main/scala/clairvoyance/scalatest/ClairvoyantContext.scala
+++ scalatest/src/main/scala/clairvoyance/scalatest/ClairvoyantContext.scala
@@ -4,23 +4,16 @@ import clairvoyance.{CapturedInputsAndOutputs, InterestingGivens}
 import clairvoyance.state.{TestState, TestStates}
 import org.scalatest._
 
-trait ClairvoyantContext extends BeforeAndAfterEachTestData
-  with OneInstancePerTest
-  with InterestingGivens
-  with CapturedInputsAndOutputs { this: Suite =>
-
-  protected def beforeExecution(): Unit = ()
-  protected def afterExecution():  Unit = ()
-
-  override protected def beforeEach(testData: TestData): Unit = beforeExecution()
-  override protected def afterEach(testData: TestData):  Unit = {
-    TestStates += (testData.name -> TestState(interestingGivens.toList, gatherCapturedValues))
-    clearCapturedValues()
-
-    afterExecution()
-  }
+trait ClairvoyantContext extends OneInstancePerTest with InterestingGivens with CapturedInputsAndOutputs { this: Suite =>
 
   implicit def stringToStep(description: String) = new ClairvoyantStep(description)
+
+  abstract override protected def runTest(testName: String, args: Args): Status = {
+    val status: Status = super.runTest(testName, args)
+    TestStates += (testName -> TestState(interestingGivens.toList, gatherCapturedValues))
+    clearCapturedValues()
+    status
+  }
 
   class ClairvoyantStep(description: String) {
     def ===>[T](step: T): T = step
