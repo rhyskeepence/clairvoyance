a1f3a229d090bf746090927472750ee5e92d701f
Rhys Keepence <rhyskeepence@gmail.com>
2014-08-31 20:44:50 +0100

    Upgrade to latest specs2/scalatest

--- project/build.scala
+++ project/build.scala
@@ -60,7 +60,7 @@ object build extends Build {
     .settings(moduleSettings: _*)
     .settings(name := "clairvoyance-specs2",
       libraryDependencies := Seq(
-        "org.specs2"     %% "specs2"     % "2.4.1",
+        "org.specs2"     %% "specs2"     % "2.4.2",
         "org.scalacheck" %% "scalacheck" % "1.11.5" % "test"
       ),
       testOptions in Test += Tests.Setup(() => {
