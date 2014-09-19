499278aed6df7d939ce8eb5ae220f8c9ed4c521b
Raymond Barlow <rbarlow@raymanoz.com>
2014-09-19 12:31:17 +0100

    Example of how to create a ScalaTest Clairyovance test, without having to specify the -C test option.

---
+++ scalatest/src/test/scala/clairvoyance/scalatest/examples/HtmlReportWithoutTestOptionExample.scala
@@ -0,0 +1,11 @@
+package clairvoyance.scalatest.examples
+
+import clairvoyance.scalatest.ClairvoyantContext
+import clairvoyance.scalatest.export.single.ClairvoyantSpec
+import org.scalatest.FunSuite
+
+class HtmlReportWithoutTestOptionExample extends FunSuite with ClairvoyantContext with ClairvoyantSpec {
+  test("This test can be run on it's own, without providing the -C test option, and an html report will still be created") {
+    assert("Istanbul" !== "Constantinople")
+  }
+}
