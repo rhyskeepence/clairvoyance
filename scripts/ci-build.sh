40943e1c70699b0e199bd6414f1e1792413fe7a6
Franck Rasolo <franck.rasolo@gmail.com>
2015-11-11 06:32:21 +0000

    Make Travis reuse the Ivy cache for faster builds

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 sbt clean coverage test
+
+# clean up to avoid unnecessary cache updates
+find $HOME/.sbt -name "*.lock" | xargs rm
+find $HOME/.ivy2 -name "ivydata-*.properties" | xargs rm
