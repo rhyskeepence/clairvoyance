30e069761b559b9fb19aae6e10f92db297223712
Franck Rasolo <franck.rasolo@gmail.com>
2016-08-11 09:59:24 +0100

    Upgraded Scala to 2.11.8

--- project/Common.scala
+++ project/Common.scala
@@ -4,12 +4,13 @@ import sbt.Keys._
 import scala.util.Try
 
 object Common {
+  val scala_2_11 = "2.11.8"
 
   lazy val settings = Seq(
     organization := "com.github.rhyskeepence",
     version := Try(sys.env("BUILD_NUMBER")).map("1.0." + _).getOrElse("1.0-SNAPSHOT"),
-    scalaVersion := "2.11.7",
-    crossScalaVersions := Seq("2.10.6", "2.11.7"),
+    scalaVersion := scala_2_11,
+    crossScalaVersions := Seq("2.10.6", scala_2_11),
     scalacOptions in GlobalScope ++= Seq(
       "-Xcheckinit", "-Xlint", "-deprecation", "-unchecked", "-feature",
       "-language:implicitConversions,reflectiveCalls,postfixOps,higherKinds,existentials"
