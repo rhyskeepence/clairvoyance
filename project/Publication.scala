c6c030f0ea39af6b2f5ffb5e476879b79d313e1b
Rhys Keepence <rhys@rhyskeepence.com>
2018-11-26 21:18:59 +0000

    upgrade sbt

--- project/Publication.scala
+++ project/Publication.scala
@@ -7,9 +7,9 @@ import scala.util.Try
 object Publication {
 
   lazy val settings = Seq(
-    publishTo <<= version { project_version ⇒
+    publishTo := {
       val nexus = "https://oss.sonatype.org/"
-      if (project_version.trim.endsWith("SNAPSHOT"))
+      if (version.value.trim.endsWith("SNAPSHOT"))
         Some("snapshots" at nexus + "content/repositories/snapshots")
       else
         Some("releases" at nexus + "service/local/staging/deploy/maven2")
--- project/Website.scala
+++ project/Website.scala
@@ -1,15 +1,6 @@
 import sbt._
 import sbt.Keys._
-import com.typesafe.sbt.SbtSite.site
-import com.typesafe.sbt.SbtSite.SiteKeys.{siteSourceDirectory, siteMappings}
 
 object Website {
 
-  lazy val settings = site.settings ++ Seq[Setting[_]](
-    siteSourceDirectory <<= baseDirectory (_ / "site"),
-    siteMappings <++= baseDirectory map { (b) =>
-      (b / "specs2" / "target" / "clairvoyance-reports" ** "*" pair rebase(b / "specs2" / "target" / "clairvoyance-reports", "clairvoyance-reports/specs2")) ++
-        (b / "scalatest" / "target" / "clairvoyance-reports" ** "*" pair rebase(b / "scalatest" / "target" / "clairvoyance-reports", "clairvoyance-reports/scalatest"))
-    }
-  )
 }
