e9fa983b5ded3ae6f0889bc458308a8ec4fbd505
Rhys Keepence <rhys@rhyskeepence.com>
2019-09-22 21:33:12 +0100

    remove ivy2 cache, replace with coursier cache, for sbt 1.3

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -2,7 +2,3 @@
 
 sbt clean coverage test coverageReport
 sbt coverageAggregate
-
-# clean up to avoid unnecessary cache updates
-find $HOME/.sbt -name "*.lock" | xargs rm
-find $HOME/.ivy2 -name "ivydata-*.properties" | xargs rm
